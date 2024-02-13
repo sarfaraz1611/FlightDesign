45597af97b7b0a4bf28eb9a95919cf1a8271661b
Mohammed Sarfaraz <50805873+sarfaraz1611@users.noreply.github.com>
2024-02-13 14:32:22 +0530

    bg color removed

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -52,7 +52,7 @@ function Navbar() {
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
             
             
             <li className="  border-b-2 pb-2 border-[#8DD3BB]">
